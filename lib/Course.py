a711fc8d47973211b333bf9513bf476e9942f5a1
Tong Tang <pichip22@hotmail.com>
2023-09-11 13:24:17 -0400

    completed

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,11 @@
 class Course:
-    pass
+    def __init__(self, title = "NULL", schedule = "NULL", description = "NULL"):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '-----------------------'
+        return output
